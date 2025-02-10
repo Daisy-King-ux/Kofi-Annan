4b9c80ad15f903734e89b958bfcbb047ba9972c6
Kofi annan <neiyianth@gmail.com>
2025-02-10 00:06:35 +0000

    added a sphere and a camera with parameters

--- hello world/src/main.js
+++ hello world/src/main.js
@@ -4,3 +4,15 @@ import * as THREE from 'three';
 const scene = new THREE.scene();
 
 //create our sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64)
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+  
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+
+//camera
+const camera = new THREE.PerspectiveCamera(45, 800, 600)
+scene.add(camera)
