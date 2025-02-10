af17fb93e81f6cee96b8e041522c8dd571e89a4f
Kofi annan <neiyianth@gmail.com>
2025-02-10 09:39:05 +0000

    hit a wall

--- hello world/src/main.js
+++ hello world/src/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-
+import "./style.css"
 //Scene
 const scene = new THREE.Scene();
 
@@ -7,22 +7,31 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64)
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
-});
+})
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
  
+//sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
 //light
 const light = THREE.PointLight(0xffffff, 1, 100);
 light.position.set(0, 10, 10);
 scene.add(light);
 
 //camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100);
 camera.position.z = 20
 scene.add(camera);
 
 //renderer
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
